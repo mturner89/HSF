7953aee3d20d144f5554d2c1d77c1d516d22786b
Michael <mturner89@users.noreply.github.com>
2016-05-12 12:45:47 -0400

    Update SurveyFunctionality1.js

--- SurveyFunctionality1.js
+++ SurveyFunctionality1.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $('#947_4840_4_19194').parent().hide();
-    console.log('js is working');
+    $('#947_4840_4_19194').closest('td').hide();
+    
     $('#947_4840_3_19193').on('change', function() {
       if ( this.value == 'Yes') {
-        $('#947_4840_4_19194').parent().show();
-        console.log('ive entered the If');
+        $('#947_4840_4_19194').closest('td').show();
       }
       else {
+          $('#947_4840_4_19194').closest('td').hide();
       }
     });
   });
