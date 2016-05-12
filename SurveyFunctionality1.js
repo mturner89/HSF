3acbadeb543693562a1d106a725792901fade992
Michael <mturner89@users.noreply.github.com>
2016-05-12 12:38:33 -0400

    Update SurveyFunctionality1.js

--- SurveyFunctionality1.js
+++ SurveyFunctionality1.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $('#947_4840_3_19193').on('change', function() {
       if ( this.value == 'Yes') {
         $('#947_4840_4_19194').parent().show();
-        console.log('i've entered the If');
+        console.log('ive entered the If');
       }
       else {
       }
