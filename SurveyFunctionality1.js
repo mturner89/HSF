9d9fff533ca854a37f7f76171010463651d8628b
Michael <mturner89@users.noreply.github.com>
2016-05-12 12:38:16 -0400

    Update and rename SurveyFunctionality.js to SurveyFunctionality1.js

--- SurveyFunctionality1.js
+++ SurveyFunctionality1.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $('#947_4840_4_19194').parent().hide();
-    
+    console.log('js is working');
     $('#947_4840_3_19193').on('change', function() {
-      if ( this.value == 'yes') {
+      if ( this.value == 'Yes') {
         $('#947_4840_4_19194').parent().show();
+        console.log('i've entered the If');
       }
       else {
       }
